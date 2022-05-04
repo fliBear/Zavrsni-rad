40bacf9c1a6de638b471e786c1c6524a16b43078
David Garić <david.garic@fer.hr>
2022-05-04 16:37:37 +0200

    Fixed single car route

--- src/pages/Car.js
+++ src/pages/Car.js
@@ -7,7 +7,7 @@ export default function Car() {
 
     useEffect(async () => {
         //Give path to data to show
-        await bob.path("http://localhost:8080/HAL-Forms/car/2");
+        await bob.path("http://localhost:8080", "cars", "car2");
         //Give instruction to build data and save inside state
         const getData = await bob.build();
         setData(getData);
